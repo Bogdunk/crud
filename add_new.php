fbc12dc361849038a6580bfbbc5126b8c47118bd
Bogdan-Emil Dragoiu <bogdanemildragoiu@gmail.com>
2025-10-07 22:39:11 +0300

    adding index.php page to project

--- add_new.php
+++ add_new.php
@@ -1,14 +1,22 @@
 <?php
 include "db_conn.php";
+if (!isset($conn)) die('Conn missing after include');
 
 if( isset($_POST['submit']) ){
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
-    $password = $_POST['password'];
+    $gender = $_POST['gender'];
 
-    //todo @bogdan minutul 12:16 => https://www.youtube.com/watch?v=siwoaqAhv3g
-    $sql = "INSERT INTO `crud`(`id`, `first_name`, `last_name`, `email`, `gender`) VALUES ('NULL','$first_name','$last_name','$email','$password')";
+    $sql = "INSERT INTO `crud`(`id`, `first_name`, `last_name`, `email`, `gender`) VALUES ('NULL','$first_name','$last_name','$email','$gender')";
+
+    $result = mysqli_query( $conn, $sql);
+    if( $result ){
+        header('Location: index.php?msg=New record created successfully');
+    }
+    else {
+        echo "Failed: " . mysqli_error($conn);
+    }
 }
 
 ?>
